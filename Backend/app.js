ae9b455aa2409595808da3691972459b22556b4b
Manish Prasad <100564494+manisprasad@users.noreply.github.com>
2025-06-22 00:47:49 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware Setup
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://axion-ai-frontend.vercel.app"], // Allow your frontend domain and others
+    origin: ["http://localhost:3000", "https://axion-ai-frontend.vercel.app", "https://axion-ai-frontend-v2.vercel.app"], // Allow your frontend domain and others
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Enable cookies in requests and responses
